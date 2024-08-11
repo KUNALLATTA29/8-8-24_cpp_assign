c52b8d5458d667342ef5fbca7a860f031f96f892
kunal latta <95677039+KUNALLATTA29@users.noreply.github.com>
2024-08-11 21:07:19 +0530

    Add files via upload

---
+++ prog_36.cpp
@@ -0,0 +1,35 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+stack<string> rev(stack<string> &st){
+    stack<string> tem;
+    while(!st.empty()){
+        tem.push(st.top());
+        st.pop();
+    }
+    st=tem;
+    return st;
+}
+
+int main(){
+    map<int,float> mymap = {
+        {1,2.3},
+        {2,66.3},
+        {3,5.9},
+        {4,9.9}
+    };
+    stack<pair<int,float>> st;
+    for(auto it:mymap){
+        st.push(it);
+    }
+    
+
+    while(!st.empty()){
+        auto k = st.top();
+        cout<<k.first<<":"<<k.second<<endl;
+        st.pop();
+    }
+    
+    return 0;
+}
